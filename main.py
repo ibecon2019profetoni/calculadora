ce36074c409a12938808c17dd0e502c31c8d8598
Toni Ferra Bonet <toniferra@MacBook-Pro-de-Toni.local>
2019-10-18 18:55:08 +0200

    botones funcionando

--- main.py
+++ main.py
@@ -11,9 +11,17 @@ def inicio():
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
+        expression = request.form.get('expression')
+        # si le dan al = sin poner nigún número vamos hacer que se ponga el 0
+        if expression == '':
+            expression = '0'
 
+        try:
+            result = eval(expression)
+        except SyntaxError:
+            result = '0'
 
-        return render_template('index.html')
+        return render_template('index.html', result=result)
 
 
 if __name__ == "__main__":
