2c68fdf4cc24b714e1f03bfddbb6584f227595b4
Toni Ferra Bonet <toniferra@MacBook-Pro-de-Toni.local>
2019-10-18 17:48:37 +0200

    nuevo concepto de calculadora con teclado

--- main.py
+++ main.py
@@ -8,23 +8,7 @@ app = Flask(__name__)
 # rutas
 @app.route('/', methods=['GET', 'POST'])
 def inicio():
-    if request.method == 'POST':
-        num01 = int(request.form['num1'])
-        num02 = int(request.form['num2'])
-        operador = request.form['operador']
-        
-        if operador == '+':
-            resultadoMAIN = num01 + num02
-        elif operador == '-':
-            resultadoMAIN = num01 - num02
-        elif operador == '*':
-            resultadoMAIN = num01 * num02
-        elif operador == '/':
-            resultadoMAIN = num01 / num02
 
-        return render_template('index.html', resultado=resultadoMAIN)
-
-    return render_template('index.html')
 
 
 if __name__ == "__main__":
